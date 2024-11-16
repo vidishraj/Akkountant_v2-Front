bafd528f5669e1b6f975ae6348c9da8474238192
vidishraj <vidishraj@gmail.com>
2024-11-17 00:23:16 +0530

    refacto: Fixed env variables for vite

--- src/components/FirebaseConfig.tsx
+++ src/components/FirebaseConfig.tsx
@@ -5,14 +5,15 @@ import {
     browserLocalPersistence,
 } from 'firebase/auth';
 
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+    apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDER,
+    appId: import.meta.env.VITE_APP_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENTID,
 };
 
 
