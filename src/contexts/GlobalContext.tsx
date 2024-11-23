02e847ff7b63fa8e92541b8d8c31c21f6a941f6d
vidishraj <vidishraj@gmail.com>
2024-11-23 16:52:59 +0530

    feat: Updated user context with optedBanks and selection

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,22 +1,29 @@
 // src/context/UserContext.tsx
-import { createContext, useContext, useState, ReactNode } from 'react';
-import { User } from 'firebase/auth';
+import {createContext, useContext, useState, ReactNode} from 'react';
+import {User} from 'firebase/auth';
 
 type UserContextType = {
     user: User | null;
     setUser: (user: User | null) => void;
+    optedBanks: string[];
+    setOptedBanks: (state: string[]) => void;
+    transactionModeSelection: boolean; //false for files, true for transactions
+    setTransactionMode: (state: boolean) => void;
 };
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
-export const UserProvider = ({ children }: { children: ReactNode }) => {
+export const UserProvider = ({children}: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
+    const [transactionModeSelection, setTransactionMode] = useState<boolean>(true);
+    const [optedBanks, setOptedBanks] = useState<string[]>([]);
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
-    {children}
-    </UserContext.Provider>
-);
+        <UserContext.Provider
+            value={{user, setUser, transactionModeSelection, setTransactionMode, optedBanks, setOptedBanks}}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 export const useUser = () => {
