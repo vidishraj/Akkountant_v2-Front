540166429cd53a55df6f0f05e3deab029309f3e0
AishwaryaDV <millamatew@gmail.com>
2024-11-25 16:27:37 +0530

    feat: investments component created and routes linked

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {setupAxiosInterceptors} from "./services/AxiosConfig.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import Transactions from "./pages/Transactions.tsx";
 import PrivateRoute from "./utils/PrivateRoute.tsx";
+import Investments from './pages/Investments/Investments.tsx';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Route path="/login" element={<Login/>}/>
             <Route path="/home" element={<PrivateRoute><HomePage/></PrivateRoute>}/>
             <Route path="/transactions" element={<PrivateRoute><Transactions/></PrivateRoute>}/>
+            <Route path="/investments" element={<PrivateRoute><Investments/></PrivateRoute>}/>
         </Routes>
     )
 }
---
+++ src/pages/Investments/Investments.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "../Investments/Investments.module.scss";
+
+const Investments = () => {
+  return (
+    <div>Investments</div>
+  )
+}
+
+export default Investments
