03b94102e8a3ebdb436ee49845d4657350804c3e
AishwaryaDV <millamatew@gmail.com>
2024-11-26 22:28:49 +0530

    feat: routes linked for home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import {useEffect} from "react";
-import {useSearchParams} from "react-router-dom";
+import {Link, useNavigate, useSearchParams} from "react-router-dom";
 import {makeDriveTokenRequest, makeTokenRequest} from "../../services/GoogleApiUtils.tsx";
 import style from "../Home/Home.module.scss";
 import Lottie from "lottie-react";
@@ -9,39 +9,6 @@ import {styled} from '@mui/system';
 import {
     Box
 } from '@mui/material';
-
-const LottieContainers = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '60vh',
-    width:"60vh",
-    overflow:"visible",
-    objectFit:"contain",
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      maxHeight: '50vh',
-      '&:first-of-type': {
-        top: 0,
-        left: 0,
-      },
-      '&:last-of-type': {
-        top: '50%',
-      },
-    },
-    [theme.breakpoints.down('md')]: {
-      display: 'flex',
-    maxHeight: '70vh',
-    overflow: 'hidden',
-      '&:first-of-type': {
-        top: 0,
-        left: 0,
-      },
-      '&:last-of-type': {
-        top: '50%',
-      },
-    },
-  }));
 
 const Home = () => {
     const [searchParams] = useSearchParams()
@@ -61,17 +28,20 @@ const Home = () => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+     const navigate=useNavigate();
 
     return (
 
         <div className={style.homeContainer}>
-            <div className={style.investmentsContainer}>
-                <Lottie animationData={homeInvestmentsAnimation} loop={true} autoPlay={true} height={100} width={100} />
-                <div className={style.investmentsTag}>Investments</div>
+            <div className={style.investmentsContainer} onClick={()=>{navigate('/investments')}}>
+               
+               <Lottie animationData={homeInvestmentsAnimation} loop={true} autoPlay={true} height={100} width={100} />
+               <div className={style.investmentsTag}>Investments</div>
             </div>
-            <div className={style.transactionsContainer}>
+            <div className={style.transactionsContainer} onClick={()=>{navigate('/transactions')}}>
                 <Lottie animationData={homeTransactionsAnimation} loop={true} autoPlay={true} height={100} width={100} />
                 <div className={style.transactionsTag}>Transactions</div>
+               
             </div>
 
         </div>
