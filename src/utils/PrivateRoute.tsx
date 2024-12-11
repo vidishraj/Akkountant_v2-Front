b5eb87538d2cbfb6eccc55e1d8af49d6ed81a76a
AishwaryaDV <millamatew@gmail.com>
2024-12-11 22:20:25 +0530

    fix: fixed route redirection error on refresh

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,14 +1,30 @@
-import {Navigate, useLocation} from "react-router-dom"
-import {auth} from '../components/FirebaseConfig.tsx';
+import React, { useEffect, useState } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { auth } from '../components/FirebaseConfig.tsx';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const PrivateRoute = (props: any) => {
-    let location = useLocation();
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+    const location = useLocation();
 
-    if (!auth.currentUser) {
-        return <Navigate to="/login" state={{from: location}} replace/>
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setIsAuthenticated(user ? true : false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+   //app will pause till firebase checks and populates user
+    if (isAuthenticated === null) {
+        return null;
     }
-    return props.children;
 
+    if (!isAuthenticated) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return props.children;
 };
 
 export default PrivateRoute;
